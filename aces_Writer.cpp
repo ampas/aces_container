8ba0982ccfb82c9cc24eb2587bddfc560007b5c3
Miaoqi Zhu <mzhu@oscars.org>
2017-10-25 10:33:53 -0700

    updates ACES output size to 350MB
    * increases ACES output size from 150MB to 350MB

--- aces_Writer.cpp
+++ aces_Writer.cpp
@@ -208,8 +208,8 @@ err aces_Writer:: configure( const MetaWriteClip & clipMeta )
 	}
 	outputBufferSize = outputRows * (8 + 2 * 4 + outputCols * hi.channels.size() * 2) + 1100000;	
 	
-    // 100e6
-	assert ( outputBufferSize < 150e6 );
+    // 150e6 to 350E06
+	assert ( outputBufferSize < 350E06 );
 	
 	delete[] pOutputBuffer;
 	pOutputBuffer = new char [ (size_t) outputBufferSize ];
