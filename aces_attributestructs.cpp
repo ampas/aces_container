c94716086e982d345e736d7a46dccaf7d36526c5
Mikael Sundell <mikael.sundell@gmail.com>
2023-06-30 18:40:20 +0200

    Fixes for better attribute writes now ignores empty values

--- aces_attributestructs.cpp
+++ aces_attributestructs.cpp
@@ -228,7 +228,6 @@ acesImageContainerFlag	( 1 ),
 pixelAspectRatio	( 1.0f ),
 screenWindowWidth	( 1.0f ),
 
-
 //	initialize optional attributes to "ignored" values
 altitude			( 0.0f ),
 aperture			( 0.0f ), 
@@ -247,11 +246,7 @@ longitude			( 0.0f ),
 orientation			( 0 ), 
 originalImageFlag	( -1 ), 
 timecodeRate		( 0 ), 
-utcOffset			( 0.0f ),
-optionalAttributes  ( false ),
-dynamicAttributes   ( false ),
-customAttributes    ( false )
-
+utcOffset			( 0.0f )
 {	
 	//	initialize required attributes to required values
 	Chromaticities			=	ChromaticitiesForACES;
